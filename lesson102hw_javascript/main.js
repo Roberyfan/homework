16184a0a837e7b5741dbea59ca7312e31520d181
Roberyfan <robery88@gmail.com>
2025-02-03 00:04:34 +0300

    Add files via upload

---
+++ lesson102hw_javascript/main.js
@@ -0,0 +1,50 @@
+const robi = {
+    name: 'Robert',
+    age: 36,
+    isAdmin: true,
+    hello(data) {
+        console.log('Hello ${data}')
+    }
+}
+
+const users = [
+    {
+        name: 'Robert',
+        age: 36,
+        isAdmin: true
+    },
+
+    {
+        name: 'Valeriya',
+        age: 28,
+        isAdmin: false
+    },
+
+    {
+        name: 'Marina',
+        age: 39,
+        isAdmin: false
+    },
+
+    {
+        name: 'Stas',
+        age: 30,
+        isAdmin: false
+    },
+
+    {
+        name: 'Vasilii',
+        age: 38,
+        isAdmin: true
+    }
+]
+
+let notadmins = 0
+
+for (let i = 0; i < users.length; i++) {
+    if (!users[i].isAdmin) {
+        notadmins = notadmins + 1
+    }
+}
+
+console.log(notadmins)
