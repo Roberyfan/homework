311a3d01db03094599289fe7801aacd3fab94901
Roberyfan <robery88@gmail.com>
2025-02-10 23:44:22 +0300

    Add files via upload

--- final lesson-2/js/main.js
+++ final lesson-2/js/main.js
@@ -167,4 +167,10 @@
         }
     });
 
+    // Маска телефона
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
